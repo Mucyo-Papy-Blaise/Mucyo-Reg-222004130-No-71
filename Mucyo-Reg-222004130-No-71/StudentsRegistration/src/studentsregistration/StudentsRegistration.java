37f15dc07f34364bc493ef01462aed31dd46ab68
Mucyo Papy Blaise <149539060+Mucyo-Papy-Blaise@users.noreply.github.com>
2023-11-16 10:08:22 +0200

    Add files via upload

---
+++ Mucyo-Reg-222004130-No-71/StudentsRegistration/src/studentsregistration/StudentsRegistration.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package studentsregistration;
+
+/**
+ *
+ * @author user
+ */
+public class StudentsRegistration {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
